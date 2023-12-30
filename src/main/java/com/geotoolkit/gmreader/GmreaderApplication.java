12730f5f5e026c6eb3837e43c01ad095d964cd52
YuraLukianchenko <qlukianchenkop@gmail.com>
2023-12-30 21:31:48 +0200

    Commit with failed build. Just for check cicd

--- src/main/java/com/geotoolkit/gmreader/GmreaderApplication.java
+++ src/main/java/com/geotoolkit/gmreader/GmreaderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmreaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GmreaderApplication.class, args);
+		SpringApplication.run(GmreaderApplicatio.class, args);
 	}
 
 }
