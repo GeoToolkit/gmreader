163b112355eea4e3b2d8ec5761de754b95a3049e
YuraLukianchenko <qlukianchenkop@gmail.com>
2023-12-30 21:39:08 +0200

    Revert "Commit with failed build. Just for check cicd"
    This reverts commit 12730f5f5e026c6eb3837e43c01ad095d964cd52.

--- src/main/java/com/geotoolkit/gmreader/GmreaderApplication.java
+++ src/main/java/com/geotoolkit/gmreader/GmreaderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmreaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GmreaderApplicatio.class, args);
+		SpringApplication.run(GmreaderApplication.class, args);
 	}
 
 }
