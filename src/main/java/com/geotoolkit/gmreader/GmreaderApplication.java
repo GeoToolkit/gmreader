9d515c797a885691dd503fcdf00b703fa1bcc274
YuraLukianchenko <qlukianchenkop@gmail.com>
2023-12-30 20:44:36 +0200

    Initial commit

---
+++ src/main/java/com/geotoolkit/gmreader/GmreaderApplication.java
@@ -0,0 +1,13 @@
+package com.geotoolkit.gmreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmreaderApplication.class, args);
+	}
+
+}
