bc34e3ca607247588f18188d9a79d74b95193ead
YuraLukianchenko <qlukianchenkop@gmail.com>
2024-01-01 15:18:31 +0200

    [GEOTOOLKIT-002]: Adds pmd plugin

--- src/main/java/com/geotoolkit/gmreader/controller/FileUploadController.java
+++ src/main/java/com/geotoolkit/gmreader/controller/FileUploadController.java
@@ -6,25 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 @Controller
 public class FileUploadController {
 
     @PostMapping("/single-file-upload")
     public ResponseEntity<Map<String, String>> handleFileUploadUsingCurl(
-            @RequestParam("file") MultipartFile file) throws IOException {
+            @RequestParam("file") MultipartFile file) {
 
         Map<String, String> map = new HashMap<>();
-
-
-        Scanner scanner = new Scanner(file.getInputStream());
-//        while(scanner.hasNextLine()) {
-//            System.out.println(scanner.nextLine());
-//        }
 
         // Populate the map with file details
         map.put("fileName", file.getOriginalFilename());
--- src/test/java/com/geotoolkit/gmreader/GmreaderApplicationTests.java
+++ src/test/java/com/geotoolkit/gmreader/GmreaderApplicationTests.java
@@ -1,13 +1,9 @@
 package com.geotoolkit.gmreader;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmreaderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
